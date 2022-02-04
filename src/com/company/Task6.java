f5640ac3cc20bff6e42b299fed87167715ed9ed2
Sona <sonmiq@gmail.com>
2022-02-04 15:24:42 +0400

    Task6 edited

--- src/com/company/Task6.java
+++ src/com/company/Task6.java
@@ -1,7 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Task6 {
     public static void main(String[] args) {
 
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Insert right-angle triangle leg1");
+        int leg1=scanner.nextInt();
+        System.out.println("Insert right-angle triangle leg2");
+        int leg2=scanner.nextInt();
+        System.out.println("area= "+(double)leg1*leg2/2);
+        System.out.println("Insert square side");
+        int side= scanner.nextInt();
+        System.out.println("area= "+side*side);
+        System.out.println("Insert rectangle length");
+        int length= scanner.nextInt();
+        System.out.println("Insert rectangle width");
+        int width= scanner.nextInt();
+        System.out.println("area= "+length*width);
     }
 }
